0b6833610b4e81c92ca310e786385724bd2bc7e1
Kaue Silveira <silveira.kaue@gmail.com>
2025-10-18 18:46:05 +0200

    color board

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import "fmt"
 
+const (
+	ColorReset   = "\033[0m"
+	ColorReverse = "\033[7m"
+)
+
 type Main struct {
 	board []string
 }
 
-func New() *Main{
+func New() *Main {
 	return &Main{board: []string{
-		"RNBQKBNR",
-		"PPPPPPPP",
+		"rnbqkbnr",
+		"pppppppp",
 		"        ",
 		"        ",
 		"        ",
@@ -20,8 +25,15 @@ func New() *Main{
 }
 
 func (m *Main) Print() {
-	for _, row := range m.board {
-		fmt.Printf("%s\n", row)
+	for i, row := range m.board {
+		for j, col := range row {
+			if (i+j)%2 == 0 {
+				fmt.Print(ColorReverse)
+			}
+			fmt.Printf("%c", col)
+			fmt.Print(ColorReset)
+		}
+		fmt.Println()
 	}
 }
 
