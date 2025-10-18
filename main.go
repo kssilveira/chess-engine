8a131bd39bea717d1afb5799aec7e1fcc9856ee2
Kaue Silveira <silveira.kaue@gmail.com>
2025-10-18 19:09:34 +0200

    add count of squares attacked by pawns

--- main.go
+++ main.go
@@ -9,10 +9,34 @@ const (
 
 type Main struct {
 	board []string
+	count [][]int
 }
 
+type Piece struct {
+	single     bool
+	value      int
+	reverse    bool
+	directions []Direction
+}
+
+type Direction struct {
+	i int
+	j int
+}
+
+var (
+	Pieces = map[rune]Piece{
+		'P': {
+			single:     true,
+			value:      1,
+			reverse:    false,
+			directions: []Direction{{-1, -1}, {-1, +1}},
+		},
+	}
+)
+
 func New() *Main {
-	return &Main{board: []string{
+	m := &Main{board: []string{
 		"rnbqkbnr",
 		"pppppppp",
 		"        ",
@@ -22,15 +46,59 @@ func New() *Main {
 		"PPPPPPPP",
 		"RNBQKBNR",
 	}}
+	m.count = make([][]int, len(m.board))
+	for i, row := range m.board {
+		m.count[i] = make([]int, len(row))
+	}
+	m.Update()
+	return m
+}
+
+func (m *Main) Update() {
+	for i, row := range m.board {
+		for j, _ := range row {
+			m.count[i][j] = 0
+		}
+	}
+
+	for i, row := range m.board {
+		for j, v := range row {
+			piece, ok := Pieces[v]
+			if !ok {
+				continue
+			}
+			for _, direction := range piece.directions {
+				for delta := 1; ; delta++ {
+					ni := i + direction.i*delta
+					nj := j + direction.j*delta
+					if ni < 0 || ni >= len(m.board) || nj < 0 || nj >= len(row) {
+						break
+					}
+					m.count[ni][nj] += piece.value
+					if piece.single {
+						break
+					}
+				}
+			}
+		}
+	}
 }
 
 func (m *Main) Print() {
 	for i, row := range m.board {
-		for j, col := range row {
+		for j, v := range row {
 			if (i+j)%2 == 0 {
 				fmt.Print(ColorReverse)
 			}
-			fmt.Printf("%c", col)
+			fmt.Printf("%c ", v)
+			fmt.Print(ColorReset)
+		}
+		fmt.Println()
+		for j, _ := range row {
+			if (i+j)%2 == 0 {
+				fmt.Print(ColorReverse)
+			}
+			fmt.Printf("%d ", m.count[i][j])
 			fmt.Print(ColorReset)
 		}
 		fmt.Println()
